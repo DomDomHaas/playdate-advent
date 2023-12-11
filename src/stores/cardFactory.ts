b3377f1e730634422ead77745bce8c0e5666ed22
Dominik Haas <shaodiese@gmail.com>
2023-12-11 23:45:43 +0100

    increased version: 1.2.0 with all assets and sfx;

--- src/stores/cardFactory.ts
+++ src/stores/cardFactory.ts
@@ -11,16 +11,16 @@ import card10 from '@/assets/cards/card=10.png';
 import card11 from '@/assets/cards/card=11.gif';
 import card12 from '@/assets/cards/card=12.png';
 import card13 from '@/assets/cards/card=13.png';
-import card14 from '@/assets/cards/card=14.png';
+import card14 from '@/assets/cards/card=14.gif';
 import card15 from '@/assets/cards/card=15.png';
 import card16 from '@/assets/cards/card=16.png';
 import card17 from '@/assets/cards/card=17.png';
 import card18 from '@/assets/cards/card=18.gif';
 import card19 from '@/assets/cards/card=19.png';
 import card20 from '@/assets/cards/card=20.png';
-import card21 from '@/assets/cards/card=21.png';
+import card21 from '@/assets/cards/card=21.gif';
 import card22 from '@/assets/cards/card=22.png';
-import card23 from '@/assets/cards/card=23.png';
+import card23 from '@/assets/cards/card=23.gif';
 import card24 from '@/assets/cards/card=24.png';
 
 export const getCards = () => {
