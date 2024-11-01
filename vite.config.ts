c352c30d9f6460ec479b583821d2c7cf670f1246
Dominik Haas <shaodiese@gmail.com>
2024-11-01 23:02:35 +0100

    added DevTools

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import {defineConfig, loadEnv} from 'vite'
 import Components from 'unplugin-vue-components/vite'
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 const version = process.env.npm_package_version;
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
+export default ({ mode } : { mode: string}) => {
 
   const env = loadEnv(mode, process.cwd())
   const buildSourceMaps = env.VITE_BUILD_SOURCEMAPS === 'true'
@@ -23,6 +24,7 @@ export default ({mode}) => {
           }),
         ],
       }),
+      vueDevTools(),      
     ],
     resolve: {
       alias: {
