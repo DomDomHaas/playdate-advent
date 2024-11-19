62bae548bb8877d2cdd02e23f8e2acdbac26b48c
Dominik Haas <shaodiese@gmail.com>
2024-11-19 09:29:19 +0100

    changed imports of assets for 2024

--- src/stores/iconFactory2024.ts
+++ src/stores/iconFactory2024.ts
@@ -1,12 +1,12 @@
 import icon1 from '@/assets/2024/icons/icon=1.png';
 import icon2 from '@/assets/2024/icons/icon=2.png';
 import icon3 from '@/assets/2024/icons/icon=3.png';
-import icon4 from '@/assets/2024/icons/icon=4.png';
+// import icon4 from '@/assets/2024/icons/icon=4.png';
 import icon5 from '@/assets/2024/icons/icon=5.png';
 import icon6 from '@/assets/2024/icons/icon=6.png';
 import icon7 from '@/assets/2024/icons/icon=7.png';
 import icon8 from '@/assets/2024/icons/icon=8.png';
-import icon9 from '@/assets/2024/icons/icon=9.png';
+// import icon9 from '@/assets/2024/icons/icon=9.png';
 import icon10 from '@/assets/2024/icons/icon=10.png';
 import icon11 from '@/assets/2024/icons/icon=11.png';
 import icon12 from '@/assets/2024/icons/icon=12.png';
@@ -15,7 +15,7 @@ import icon14 from '@/assets/2024/icons/icon=14.png';
 import icon15 from '@/assets/2024/icons/icon=15.png';
 import icon16 from '@/assets/2024/icons/icon=16.png';
 import icon17 from '@/assets/2024/icons/icon=17.png';
-import icon18 from '@/assets/2024/icons/icon=18.gif';
+import icon18 from '@/assets/2024/icons/icon=18.png';
 import icon19 from '@/assets/2024/icons/icon=19.png';
 import icon20 from '@/assets/2024/icons/icon=20.png';
 import icon21 from '@/assets/2024/icons/icon=21.png';
@@ -27,8 +27,12 @@ import icon25 from '@/assets/2024/icons/icon=25.png';
 export const getIcons = () => {
 
   return [
-    icon1, icon2, icon3, icon4, icon5, icon6,
-    icon7, icon8, icon9, icon10, icon11, icon12,
+    icon1, icon2, icon3,
+    // icon4,
+    icon5, icon6,
+    icon7, icon8,
+    // icon9,
+    icon10, icon11, icon12,
     icon13, icon14, icon15, icon16, icon17, icon18,
     icon19, icon20, icon21, icon22, icon23, icon24,
     icon25,
--- src/stores/screenshotFactory2024.ts
+++ src/stores/screenshotFactory2024.ts
@@ -18,92 +18,92 @@ export const getScreenshots = () : string[] => {
   pngGlob = import.meta.glob('@/assets/2024/3/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/4/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/4/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/4/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/4/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/5/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/5/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/5/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/5/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/6/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/6/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/6/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/6/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
   gifGlob = import.meta.glob('@/assets/2024/7/*.gif', { eager: true });
   pngGlob = import.meta.glob('@/assets/2024/7/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/8/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/8/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/8/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/8/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/9/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/9/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/9/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/9/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/10/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/10/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/10/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/10/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/11/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/11/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/11/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/11/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/12/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/12/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/12/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/12/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/13/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/13/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/13/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/13/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/14/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/14/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/14/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/14/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/15/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/15/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/15/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/15/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/16/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/16/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/16/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/16/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/17/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/17/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/17/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/17/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/18/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/18/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/18/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/18/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/19/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/19/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/19/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/19/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/20/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/20*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/20/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/20*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/21/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/21/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/21/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/21/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/22/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/22/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/22/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/22/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
   gifGlob = import.meta.glob('@/assets/2024/23/*.gif', { eager: true });
   pngGlob = import.meta.glob('@/assets/2024/23/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/24/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/24/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/24/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/24/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
-  gifGlob = import.meta.glob('@/assets/2023/25/*.gif', { eager: true });
-  pngGlob = import.meta.glob('@/assets/2023/25/*.png', { eager: true });
+  gifGlob = import.meta.glob('@/assets/2024/25/*.gif', { eager: true });
+  pngGlob = import.meta.glob('@/assets/2024/25/*.png', { eager: true });
   screenshots.push([...addImages(gifGlob, pngGlob)]);
 
 /*
