03c274fb37c9a4909c9a982bf8a2d87fa3e2ee76
DomHaas <shaodiese@gmail.com>
2024-11-19 14:35:00 +0100

    added assets 1, 6

--- src/stores/cardFactory2024.ts
+++ src/stores/cardFactory2024.ts
@@ -1,7 +1,7 @@
 import card1 from '@/assets/2024/cards/card=1.png';
 import card2 from '@/assets/2024/cards/card=2.png';
 import card3 from '@/assets/2024/cards/card=3.png';
-// import card4 from '@/assets/2024/cards/card=4.png';
+import card4 from '@/assets/2024/cards/card=4.png';
 import card5 from '@/assets/2024/cards/card=5.png';
 import card6 from '@/assets/2024/cards/card=6.png';
 import card7 from '@/assets/2024/cards/card=7.png';
@@ -15,7 +15,7 @@ import card14 from '@/assets/2024/cards/card=14.gif';
 import card15 from '@/assets/2024/cards/card=15.png';
 import card16 from '@/assets/2024/cards/card=16.png';
 import card17 from '@/assets/2024/cards/card=17.png';
-// import card18 from '@/assets/2024/cards/card=18.gif';
+import card18 from '@/assets/2024/cards/card=18.png';
 import card19 from '@/assets/2024/cards/card=19.png';
 import card20 from '@/assets/2024/cards/card=20.png';
 import card21 from '@/assets/2024/cards/card=21.png';
@@ -27,13 +27,13 @@ import card25 from '@/assets/2024/cards/card=25.png';
 export const getCards = () => {
 
   return [
-    card1, card2, card3, // card4,
+    card1, card2, card3, card4,
     card5, card6,
     card7, card8, // card9,
     card10, // card11,
     // card12,
     card13, card14, card15, card16, card17,
-    // card18,
+    card18,
     card19, card20, card21, card22, card23, card24,
     card25,
   ];
--- src/stores/iconFactory2024.ts
+++ src/stores/iconFactory2024.ts
@@ -1,12 +1,12 @@
 import icon1 from '@/assets/2024/icons/icon=1.png';
 import icon2 from '@/assets/2024/icons/icon=2.png';
 import icon3 from '@/assets/2024/icons/icon=3.png';
-// import icon4 from '@/assets/2024/icons/icon=4.png';
+import icon4 from '@/assets/2024/icons/icon=4.png';
 import icon5 from '@/assets/2024/icons/icon=5.png';
 import icon6 from '@/assets/2024/icons/icon=6.png';
 import icon7 from '@/assets/2024/icons/icon=7.png';
 import icon8 from '@/assets/2024/icons/icon=8.png';
-// import icon9 from '@/assets/2024/icons/icon=9.png';
+import icon9 from '@/assets/2024/icons/icon=9.png';
 import icon10 from '@/assets/2024/icons/icon=10.png';
 import icon11 from '@/assets/2024/icons/icon=11.png';
 import icon12 from '@/assets/2024/icons/icon=12.png';
@@ -28,10 +28,10 @@ export const getIcons = () => {
 
   return [
     icon1, icon2, icon3,
-    // icon4,
+    icon4,
     icon5, icon6,
     icon7, icon8,
-    // icon9,
+    icon9,
     icon10, icon11, icon12,
     icon13, icon14, icon15, icon16, icon17, icon18,
     icon19, icon20, icon21, icon22, icon23, icon24,
